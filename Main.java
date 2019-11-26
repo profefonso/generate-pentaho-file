58c303b2cbda60877f827d13b0b444155878372f
Alfonso Caro Portillo <alfonsocarop@gmail.com>
2019-11-26 13:58:21 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args){
         InspectJson inspectJson = new InspectJson();
-        ArrayList listTables = inspectJson.getTablesArray("D:\\acaropo\\descargas\\data.json");
-        listTables.forEach((n) -> System.out.println(n));
+        ArrayList<String> listTables = inspectJson.getTablesArray("D:\\acaropo\\descargas\\data.json");
+        for (String temp : listTables) {
+            System.out.println(temp);
+        }
     }
 }
